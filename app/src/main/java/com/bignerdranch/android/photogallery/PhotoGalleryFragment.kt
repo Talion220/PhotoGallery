14aacdc09a2b3e43ce5eb8c3c10e87406f4bac57
Владислав Харченко <vladislav.harchenko2017@yandex.ru>
2023-10-11 23:54:37 +0700

    search created

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -3,10 +3,14 @@ package com.bignerdranch.android.photogallery
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -29,8 +33,33 @@ class PhotoGalleryFragment : Fragment() {
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        photoGalleryViewModel =
-            ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+        setHasOptionsMenu(true)
+        photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_photo_gallery, menu)
+        val searchItem: MenuItem = menu.findItem(R.id.menu_item_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.apply {
+            setOnQueryTextListener(object :
+                SearchView.OnQueryTextListener {
+                override fun
+                        onQueryTextSubmit(queryText: String): Boolean {
+                    Log.d(TAG,
+                        "QueryTextSubmit: $queryText")
+                    photoGalleryViewModel.fetchPhotos(queryText)
+                    return true
+                }
+                override fun
+                        onQueryTextChange(queryText: String): Boolean {
+                    Log.d(TAG,
+                        "QueryTextChange: $queryText")
+                    return false
+                }
+            })
+        }
     }
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,15 +1,17 @@
 package com.bignerdranch.android.photogallery.api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface FlickrApi {
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-        "&api_key=1f674a7dcb802f392d1851c64fa72e13" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
+    @GET("services/rest?method=flickr.interestingness.getList")
+        fun fetchUrlBytes(@Url url: String):
+                Call<ResponseBody>
+        @GET("services/rest?method=flickr.photos.search")
+            fun searchPhotos(@Query("text") query:
+                             String): Call<FlickrResponse>
     fun fetchPhotos(): Call<FlickrResponse>
 }
